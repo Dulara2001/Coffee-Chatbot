a54e38990a778cda025439d4a165416f7750ba0d
Dulara Rathnayake <87459380+Dulara2001@users.noreply.github.com>
2024-11-22 09:47:02 +0530

    create chat.py

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ learning_rate = 0.001
 input_size = len(X_train[0])
 hidden_size = 8
 output_size = len(tags)
-print(input_size, output_size)
+# print(input_size, output_size)
 
 class ChatDataset(Dataset):
 
@@ -113,3 +113,17 @@ for epoch in range(num_epochs):
 
 
 print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
